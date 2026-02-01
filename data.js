8cfd0d48060cc0041c6bd16acda6dc1df42ae1db
227y42nr49-cpu <227y42nr49@privaterelay.appleid.com>
2026-02-01 19:50:04 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 const stands = {
-"1": {name: "Autohaus Müller", area: "KFZ & Technik"},
-"2": {name: "IT Solutions GmbH", area: "Informatik"},
-"3": {name: "Klinikum Nord", area: "Gesundheit & Pflege"},
-"4": {name: "Handwerkskammer", area: "Handwerk"},
-"5": {name: "Stadtwerke Beispielstadt", area: "Energie & Umwelt"}
-// ... bis Stand 60
+  "1": {name: "Autohaus Müller", area: "KFZ & Technik"},
+  "2": {name: "IT Solutions GmbH", area: "Informatik"},
+  "3": {name: "Klinikum Nord", area: "Gesundheit & Pflege"},
+  "4": {name: "Handwerkskammer", area: "Handwerk"},
+  "5": {name: "Stadtwerke Beispielstadt", area: "Energie & Umwelt"}
+
 };
